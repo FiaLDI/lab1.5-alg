5a641593523f98c907fb766555b25e4fcd791dd7
FiaLDI <leon.youtube2017@yandex.ru>
2023-10-07 10:34:29 +0300

    add code

---
+++ prog/puzSr.py
@@ -0,0 +1,30 @@
+def sortiSR(array) -> None:
+    for i in range(len(array) - 1):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+if __name__ == '__main__':
+    from random import randint
+    from math import sqrt
+    import time
+    e = []
+    sigma = []
+    d = 0
+    q = 0
+    for i in range(100, 1000, 100):
+        for l in range(1, 31):
+            a = [0 * t for t in range(i)]
+            for k in range(i):
+                a.append(randint(100, 1000))
+            start = time.perf_counter()
+            p = sortiSR(a)
+            end = time.perf_counter()
+            d += (end-start)/30
+            q += ((end - start) - (d*30)) ** 2
+        e.append(d)
+        sigma.append(sqrt(1/30*q))
+        d = 0
+        q = 0
+    print(e)
+    print(sigma)
